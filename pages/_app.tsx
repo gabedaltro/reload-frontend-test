39010ec6ab80968f54f79888fd229392ebd37bc5
Gabriel <gabeguimaraes13@gmail.com>
2022-09-21 11:06:32 -0300

    Ajusta _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ReactElement } from "react";
+import App from "app";
+import Head from "next/head";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps): ReactElement {
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+      </Head>
+      <App>
+        <Component {...pageProps} />
+      </App>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
+
